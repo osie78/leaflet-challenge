8be9d980764a847cd111cea89740117a2bf0132c
osie78 <oscarsanguino@hotmail.com>
2021-05-18 08:36:41 -0500

    8:36

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,13 +1,12 @@
 // Creating our initial map object
-// We set the longitude, latitude, and the starting zoom level for sf
-// This gets inserted into the div with an id of 'map' in index.html
+// Centered in Colorado with a zoom that allows to cover most of the United States, recognizing that the focus area is the West Coast
 var myMap = L.map("map", {
   center: [37.7392, -104.99],
   zoom: 4.3
 });
 
-// // Adding a tile layer (the background map image) to our map
-// // We use the addTo method to add objects to our map
+// Adding the tile layer or base layer for the subsequent map additions
+// 
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
@@ -17,7 +16,7 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
-// // Store our API endpoint
+// Setting a d3.jason response function to start bringing data to the map. All other layers will be added inside this function
 var queryUrl = "https:earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 d3.json(queryUrl).then(function(response) {
