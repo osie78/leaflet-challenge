5f8ecb10c4bbfe745dd28ab2c24310d4351d7066
osie78 <oscarsanguino@hotmail.com>
2021-05-18 06:47:52 -0500

    starting

---
+++ static/js/logic.js
@@ -0,0 +1,23 @@
+// Creating our initial map object
+// We set the longitude, latitude, and the starting zoom level for sf
+// This gets inserted into the div with an id of 'map' in index.html
+var myMap = L.map("map", {
+  center: [37.7749, -122.4194],
+  zoom: 5
+});
+
+// // Adding a tile layer (the background map image) to our map
+// // We use the addTo method to add objects to our map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// // Store our API endpoint
+var queryUrl = "https:earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+
