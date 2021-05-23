8d4190400e6c300493ac5a485427b6782806913e
osie78 <oscarsanguino@hotmail.com>
2021-05-23 12:20:01 -0500

    5/23 12:19

--- static/js/logic.js
+++ static/js/logic.js
@@ -7,13 +7,27 @@ var myMap = L.map("map", {
 
 // Adding the tile layer or base layer for the subsequent map additions
 // 
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var street = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
   id: "mapbox/streets-v11",
   accessToken: API_KEY
+}).addTo(myMap);
+
+var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.satellite",
+  accessToken: API_KEY
+});
+
+var maptype={"Satellite View": satellite, "Map View":street};
+var original ={"Eartquake Magnitude": street};
+
+L.control.layers(maptype, original, {
+  collapsed: false
 }).addTo(myMap);
 
 // Setting a d3.json response function to start bringing data to the map. All other layers will be added inside this function
@@ -46,11 +60,11 @@ d3.json(queryUrl).then(function(response) {
     case magnitude > 4:
       return "#ff0066";
     case magnitude > 3:
-      return "#ff66ff";
+      return "#ff9966";
     case magnitude > 2:
-      return "#99ccff";
+      return "#ffff66";
     case magnitude > 1:
-      return "#66ffff";
+      return "#99ff33";
     default:
       return "#ffffff";
     }}
